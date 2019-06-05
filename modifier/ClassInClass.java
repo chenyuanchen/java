6c2e41d21b0c6c009263e286bcecfd17b9edd764
chenyuanchen <113367755@qq.com>
2019-06-05 15:44:58 +0800

    how to use class in class

--- modifier/ClassInClass.java
+++ modifier/ClassInClass.java
@@ -1,3 +1,24 @@
 /**
  * 类中类的用法
  */
+/**
+ * ClassInClass
+ */
+public class ClassInClass {
+
+    public static void main(String[] args) {
+        System.out.println("ClassInClass main.");
+        ClassInClass cic = new ClassInClass();
+        ClassInClass.InnerClassInClass icic = cic.new InnerClassInClass();
+        icic.function();
+    }
+
+    /**
+     * InnerClassInClass
+     */
+    public class InnerClassInClass {
+        void function(){
+            System.out.println("InnerClassInClass");
+        }
+    }
+}
